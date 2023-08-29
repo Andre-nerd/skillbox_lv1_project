346d692c6a81895105715e9f8572b82670b22902
Andrey Boukreev <ruzoomparty@gmail.com>
2023-08-29 17:58:03 +0300

    Lemma and Index write OK | deleted utis

--- searchengine-master/src/main/java/searchengine/controllers/DefaultController.java
+++ searchengine-master/src/main/java/searchengine/controllers/DefaultController.java
@@ -3,35 +3,14 @@ package searchengine.controllers;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import searchengine.config.SitesList;
-import searchengine.controllers.Utils.WriteToBD;
-import searchengine.model.Lemma;
-import searchengine.model.PageModel;
-import searchengine.model.SiteModel;
-import searchengine.model.SiteStatus;
-import searchengine.repositories.LemmaRepository;
-import searchengine.repositories.PageModelRepository;
-import searchengine.repositories.SiteModelRepository;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Transactional
 public class DefaultController {
-    private final WriteToBD writeToBD;
-
-
     Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
     /**
