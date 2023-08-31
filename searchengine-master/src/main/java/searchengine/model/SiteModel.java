85c2a553213cecd2b5f4b38821fd86ab4868fc13
Andrey Boukreev <ruzoomparty@gmail.com>
2023-08-31 12:24:30 +0300

    Project finished | ref

--- searchengine-master/src/main/java/searchengine/model/SiteModel.java
+++ searchengine-master/src/main/java/searchengine/model/SiteModel.java
@@ -43,3 +43,13 @@ public class SiteModel {
     @OneToMany(mappedBy = "owner")
     private List<Lemma> lemmas;
 }
+/**
+ * CREATE TABLE Site(
+ * id INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+ * `status` ENUM('INDEXING', 'INDEXED', 'FAILED') NOT NULL,
+ * status_time DATETIME NOT NULL,
+ * last_error TEXT,
+ * url VARCHAR(255) NOT NULL,
+ * name VARCHAR(255) NOT NULL
+ * );
+ */
